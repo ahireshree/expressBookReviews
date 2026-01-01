959811e8f576821cf851d72b88bc38e7fcd49cb0
ahireshree <shreehari.ahire@gmail.com>
2026-01-01 05:18:29 -0500

    get book by isbn and author code added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,14 +30,22 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const book = books[req.params.isbn];
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+    res.status(200).json(book);  
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author.toLowerCase();
+
+    const result = Object.values(books).filter(
+        book => book.author.toLowerCase() === author
+    );
+
+    res.status(200).json(result);  
 });
 
 // Get all books based on title
