f535f5083052ca170b4ba3babd9f6fbbc436ad0c
ahireshree <shreehari.ahire@gmail.com>
2026-01-01 07:30:54 -0500

    get book by isbn updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,14 +39,30 @@ public_users.get('/', async (req, res) => {
     }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const book = books[req.params.isbn];
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
+// Get book details based on ISBN using async/await + Axios
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try {
+        const isbn = req.params.isbn;
+
+        // Fetch all books asynchronously
+        const response = await axios.get('http://localhost:5000/');
+        const booksData = response.data;
+
+        const book = booksData[isbn];
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        res.status(200).json(book);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching book",
+            error: error.message
+        });
     }
-    res.status(200).json(book);  
 });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
